19e94b12b645a463994bae357eef73125df9837b
yhunadev <yvillas23@gmail.com>
2025-09-25 16:27:05 +0800

    feat(pig-game): implement win conditions and complete game reset functionality

--- 06-pig-game/starter/script.js
+++ 06-pig-game/starter/script.js
@@ -27,6 +27,8 @@ const btnRoll = document.querySelector('.btn--roll');
 
 const btnHoldEl = document.querySelector('.btn--hold');
 
+const btnNewEl = document.querySelector('.btn--new');
+
 // game initialization function
 
 const init = function () {
@@ -51,6 +53,14 @@ const init = function () {
   current1El.textContent = 0;
 
   diceEl.classList.add('hidden');
+
+  player0El.classList.remove('player--winner');
+
+  player1El.classList.remove('player--winner');
+
+  player0El.classList.add('player--active');
+
+  player1El.classList.remove('player--active');
 };
 init();
 
@@ -85,10 +95,23 @@ const switchPlayer = function () {
   player1El.classList.toggle('player--active');
 };
 
-btnRoll.addEventListener('click', function () {
+btnHoldEl.addEventListener('click', function () {
   if (playing && currentScore > 0) {
     scores[activePlayer] += currentScore;
     document.getElementById(`score--${activePlayer}`).textContent =
       scores[activePlayer];
+    if (scores[activePlayer] >= 100) {
+      playing = false;
+      diceEl.classList.add('hidden');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+    } else {
+      switchPlayer();
+    }
   }
 });
+btnNewEl.addEventListener('click', init);
